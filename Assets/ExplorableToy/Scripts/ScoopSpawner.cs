62781745409764088529453434477c84bd0d36de
DESKTOP-M615581\Matteo <mpscordino@hotmail.com>
2025-02-21 01:32:04 -0500

    added fully functional slider

--- Assets/ExplorableToy/Scripts/ScoopSpawner.cs
+++ Assets/ExplorableToy/Scripts/ScoopSpawner.cs
@@ -5,14 +5,20 @@ using UnityEngine.UI;
 
 public class ScoopSpawner : MonoBehaviour
 {
+
+    // public gameobjects to reference in inspector
     public GameObject chocolateScoop;
     public GameObject vanillaScoop;
     public GameObject strawberryScoop;
+    // will be the spawned objects
     GameObject chocScoop;
     GameObject vanilScoop;
     GameObject strawScoop;
 
+    // determines which button is pushed
     public int scoopNum = 0;
+    // reference to the slider in the inspector
+    public Slider slider;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,11 +28,24 @@ public class ScoopSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // constantly sets the scale of each spawned scoop to the slider value. But only if they are not null to prevent null ref error 
+        if (strawScoop != null)
+        {
+            strawScoop.transform.localScale = new Vector3(slider.value, slider.value, slider.value);
+        }
+        if (chocScoop != null)
+        {
+            chocScoop.transform.localScale = new Vector3(slider.value, slider.value, slider.value);
+        }
+        if (vanillaScoop != null)
+        {
+            vanilScoop.transform.localScale = new Vector3(slider.value, slider.value, slider.value);
+        }
     }
 
-    public void spawnScoop(int flavour)
+    public void spawnScoop(int flavour) // takes an argument determined by the button pushed
     {
+        // for all, if button is pushed, destroy all other instances of a scoop prefab, spawn a new one.
         if (flavour == 1)
         {
             Destroy(vanilScoop);
@@ -48,5 +67,13 @@ public class ScoopSpawner : MonoBehaviour
             Destroy(strawScoop); 
             strawScoop = Instantiate(strawberryScoop);
         }
+    }
+
+    public void destroyAll()
+    {
+        Destroy(chocScoop);
+        Destroy(vanilScoop);
+        Destroy(strawScoop);
+        slider.value = slider.minValue;
     }
 }
