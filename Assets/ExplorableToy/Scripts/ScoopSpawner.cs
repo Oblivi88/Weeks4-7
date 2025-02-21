7456dd8edbcbd3332160d6e8400af61c2f616b21
DESKTOP-M615581\Matteo <mpscordino@hotmail.com>
2025-02-21 00:54:05 -0500

    finally able to work again, i added functionality to the buttons. spawned scoops.

--- Assets/ExplorableToy/Scripts/ScoopSpawner.cs
+++ Assets/ExplorableToy/Scripts/ScoopSpawner.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoopSpawner : MonoBehaviour
 {
+    public GameObject chocolateScoop;
+    public GameObject vanillaScoop;
+    public GameObject strawberryScoop;
+    GameObject chocScoop;
+    GameObject vanilScoop;
+    GameObject strawScoop;
+
+    public int scoopNum = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +23,30 @@ public class ScoopSpawner : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void spawnScoop(int flavour)
+    {
+        if (flavour == 1)
+        {
+            Destroy(vanilScoop);
+            Destroy(strawScoop);
+            Destroy(chocScoop);
+            chocScoop = Instantiate(chocolateScoop);
+        }
+        if (flavour == 2)
+        {
+            Destroy(strawScoop);
+            Destroy(chocScoop);
+            Destroy(vanilScoop);
+            vanilScoop = Instantiate(vanillaScoop);
+        }
+        if (flavour == 3)
+        {
+            Destroy(chocScoop);
+            Destroy(vanilScoop);
+            Destroy(strawScoop); 
+            strawScoop = Instantiate(strawberryScoop);
+        }
     }
 }
